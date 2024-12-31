c45a00a98386bf1a757f885252f9835b1bdecd0e
Raul Harrington Jr. <raulgharrington@gmail.com>
2024-12-31 17:48:40 -0500

    ml/model.py completed

--- ml/model.py
+++ ml/model.py
@@ -2,6 +2,7 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+from sklearn.ensemble  import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -20,7 +21,11 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
-    pass
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+    return model
+
 
 
 def compute_model_metrics(y, preds):
@@ -60,7 +65,10 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
-    pass
+    preds = model.predict(X)
+
+    return preds
+
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -73,12 +81,31 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
-    pass
+    # We'll use try & except
+    try:
+        # We need the path, open file in write binary, and pickle dump
+        with open(path, 'wb') as file:
+            pickle.dump(model, file)
+        print(f"Model save to {path}")
+    
+    except Exception as e:
+        print(f"Model NOT saved! Error {e}")
+    
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
-    pass
+    # We'll use try & except again
+    try:
+        # We need the path, open file in read-binart, and pickle load
+        with open(path, 'rb') as file:
+            model = pickle.load(file)
+        print(f"Model loaded from {path}")
+
+        return model
+    
+    except Exception as e:
+        print(f"Model NOT loaded! Error {e}")
 
 
 def performance_on_categorical_slice(
@@ -118,11 +145,15 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
+    input_data = data[data[column_name] == slice_value]
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        data=input_data,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb,
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
